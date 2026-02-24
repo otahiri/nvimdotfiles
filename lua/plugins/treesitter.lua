29d818b4a3bbfa1d1db0b964ad014162de8e79cb
otahiri <chucy0066@gmail.com>
2026-02-24 21:53:19 +0000

    more stuff  for the treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,48 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  build = ":TSUpdate",
-  config = function()
-    -- Force compilation from source to avoid GLIBC mismatch
-    require("nvim-treesitter.install").prefer_git = true
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	build = ":TSUpdate",
+	config = function()
+		-- Force compilation from source to avoid GLIBC mismatch
+		require("nvim-treesitter.install").prefer_git = true
 
-    -- Use a protected call to handle the "module not found" issue gracefully
-    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-    if not status_ok then
-      return
-    end
+		-- Use a protected call to handle the "module not found" issue gracefully
+		local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+		if not status_ok then
+			return
+		end
 
-    configs.setup({
-      -- Recommended to ensure these are always present
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "python",
-        "rust",
-        "markdown",
-        "markdown_inline"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["ib"] = "@block.inner",
-            ["ab"] = "@block.outer",
-          },
-        },
-      },
-    })
-  end,
+		configs.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"python",
+				"rust",
+				"markdown",
+				"markdown_inline"
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						['af'] = '@function.outer',
+						['if'] = '@function.inner',
+						['ac'] = '@class.outer',
+						['ic'] = '@class.inner',
+						['aa'] = '@parameter.outer',
+						['ia'] = '@parameter.inner',
+					},
+				},
+			},
+		})
+	end,
 }
