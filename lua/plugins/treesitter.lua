7bed3c7f6dae8f5df1a5351cfc39fb1a34b35149
otahiri <chucy0066@gmail.com>
2026-02-24 23:12:34 +0000

    added more movements to the treesitter  need to figure out why it does not register the movements

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -26,20 +26,50 @@ return {
 				"markdown",
 				"markdown_inline"
 			},
+			auto_install = true,
 			sync_install = false,
 			highlight = { enable = true },
 			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
 			textobjects = {
 				select = {
 					enable = true,
 					lookahead = true,
 					keymaps = {
-						['af'] = '@function.outer',
-						['if'] = '@function.inner',
-						['ac'] = '@class.outer',
-						['ic'] = '@class.inner',
-						['aa'] = '@parameter.outer',
-						['ia'] = '@parameter.inner',
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = "@class.outer",
+					},
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
 					},
 				},
 			},
