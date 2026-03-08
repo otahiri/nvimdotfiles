999eb79384630e246239b7386315e6f416d96990
otahiri <chucy0066@gmail.com>
2026-03-08 18:08:02 +0000

    changed the quote in the space w save to double quotes

--- lua/custom/configs/keymaps.lua
+++ lua/custom/configs/keymaps.lua
@@ -17,7 +17,7 @@ local function format()
   end
 end
 
-vim.keymap.set('n', '<leader>w', function() vim.cmd 'silent! w' end)
+vim.keymap.set('n', '<leader>w', function() vim.cmd "silent! w" end)
 vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to upper window' })
